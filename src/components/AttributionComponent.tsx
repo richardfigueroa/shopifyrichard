954a100891f4bef401ae27bb4718294652f7c377
richard figueroa <richard.figueroa@live.com>
2021-05-09 21:15:33 -0400

    update styling

--- src/components/AttributionComponent.tsx
+++ src/components/AttributionComponent.tsx
@@ -1,7 +1,7 @@
 const AttributionComponent = () => {
   return (
     <div className={"attribution-container"}>
-      <div>Created by: Daniel Figueroa in Canada. 🇨🇦</div>
+      <div>Author: Richard Figueroa.</div>
       <div>
         Powered by <a href="http://www.omdbapi.com/">OMDB API</a>.
       </div>
@@ -14,6 +14,8 @@ const AttributionComponent = () => {
         <a href="https://www.flaticon.com/" title="Flaticon">
           www.flaticon.com
         </a>
+        <div>Font made from <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>is licensed by CC BY 3.0</div>
+
       </div>
     </div>
   );
--- src/components/SearchContainerComponent.tsx
+++ src/components/SearchContainerComponent.tsx
@@ -9,7 +9,7 @@ const SearchContainerComponent = ({ input, onChange }: IProps) => {
   return (
     <div className={"search-container"}>
       <div className={"search-title-label"}>Welcome to the Shoppies Award page!</div>
-      <div className={"search-title-label"}>Here you can nomiate up to 5 movies for the award show.</div>
+      <div className={"search-title-label"}>Here you can nominate up to 5 movies for the award show.</div>
       <div className={"search-title-label"}>Use the searchbar below to find and nomiate your favorite movie. </div>
       <SearchBarComponent input={input} onChange={onChange} />
     </div>
